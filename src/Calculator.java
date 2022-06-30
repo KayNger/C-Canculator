736eedd67396884c8c8020afd6f0c1a959eeba22
KayNger <canhlong03@gmail.com>
2022-06-30 17:15:15 +0700

    comit test

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class Calculator {
     static ArrayList<Integer> arr = new ArrayList<>();
@@ -55,7 +57,7 @@ public class Calculator {
         }
 
         for (String s : arrString) {
-            for (Integer i : findNumber(S)) {
+            for (Integer i : findNumber(s)) {
                 if (i > 1000) i = 0;
                 else if (i < 0) {
                     throw new Exception("include all of the negative number provided");
@@ -65,6 +67,28 @@ public class Calculator {
         }
     }
 
-    
+    private static int isDelimiter(String input, String[] delimiters, int i) {
+        for (String delimiter : delimiters) {
+            int deLength = isDelimiter(input, delimiters, i);
+            if (deLength > 0)
+                return deLength;
+        }
+        return 0;
+    }
+
+    private static int isDelimiter(String input, String delimiter, int i) {
+        if (delimiter.length() > input.length() - i) {
+            return 0;
+        }
+        int index = 0;
+        for (int j = i; j < input.length(); j++) {
+            if (input.charAt(j) != delimiter.charAt(index))
+                return 0;
+            index++;
+            if (delimiter.length() == index)
+                return delimiter.length();
+        }
+        return delimiter.length();
+    }
 
 }
