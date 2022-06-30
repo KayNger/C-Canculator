8cf77ec334fc03859d8b0bb7f234c05a6eee4be5
longnc <canhlong03@gmail.com>
2022-06-30 12:58:07 +0700

    comit test

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,4 +4,5 @@ public class Calculator {
     static ArrayList<Integer> arr = new ArrayList<>();
 
     public static String readFromInputStream(String path)
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String input = Calculator.readFromInputStream("input.txt");
         String[] delimiters = new String[]{"'"};
+        System.out.println(Calculator.calculate(input, delimiters));
+    }
 }
