3d78a5a2814200c38290fd7c95bac3d1ee224042
KayNger <canhlong03@gmail.com>
2022-06-30 14:51:47 +0700

    comit test

--- src/Calculator.java
+++ src/Calculator.java
@@ -25,7 +25,46 @@ public class Calculator {
     public static int calculate(String input,String[] delimiters) throws Exception {
         handleCharacter(input, delimiters);
         try {
-
+            if (arr.stream().count() == 0) {
+                return 0;
+            }
+            return arr.stream().reduce(0, Integer::sum);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
         }
     }
+
+    public static void handleCharacter(String input, String[] delimiters) throws Exception {
+        ArrayList<String> arrString = new ArrayList<>();
+        StringBuilder sb = new StringBuilder(input.length());
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            int delimiterLength = isDelimiter(input, delimiters, i);
+            if (delimiterLength > 0) {
+                arrString.add(sb.toString());
+                sb.setLength(0);
+                i = i + delimiterLength - 1;
+            } else {
+                if (i == input.length() -1 && != sb.toString().isEmpty()) {
+                    arrString.add(sb.toString());
+                } else {
+                    sb.append(c);
+                }
+            }
+        }
+
+        for (String s : arrString) {
+            for (Integer i : findNumber(S)) {
+                if (i > 1000) i = 0;
+                else if (i < 0) {
+                    throw new Exception("include all of the negative number provided");
+                }
+                arr.add(i);
+            }
+        }
+    }
+
+    
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String input = Calculator.readFromInputStream("input.txt");
         String[] delimiters = new String[]{"'"};
         System.out.println(Calculator.calculate(input, delimiters));
