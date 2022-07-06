3adfe1342559dcbf8377fd58538a295c425af6ce
KayNger <canhlong03@gmail.com>
2022-07-06 09:58:52 +0700

    comit test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        String input = Calculator.readFromInputStream("texf.txt");
+        String input = Calculator.readFromInputStream("text.txt");
         String[] delimiters = new String[]{"'"};
         System.out.println(Calculator.calculate(input, delimiters));
     }
